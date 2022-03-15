d7b512a2761783a84dfb34c8a18a8ce099a4454f
Nicholas Frederiksen <nikko.frederiksen@gmail.com>
2022-03-15 11:59:18 +0100

    chore: update unit tests

--- src/shared/utils.test.ts
+++ src/shared/utils.test.ts
@@ -5,12 +5,40 @@ describe("shared.utils", () => {
   describe("proxyPathBuilder", () => {
     it("should return correct format with value in all parameters", () => {
       // Arrange
-      const itemUri = "itemUri";
-      const urlSearchParams = new URLSearchParams("url=https://something.se");
+      const itemUri = "video-variants/variant_3.m3u8";
+      const urlSearchParams = new URLSearchParams("url=https://mock.stream.origin.se/hls/vods/asset41/master.m3u8&delay=[{i:3,ms:200}]");
 
       // Act
       const actual = proxyPathBuilder(itemUri, urlSearchParams, "proxy-media");
-      const expected = "proxy-media?url=something.se%2FitemUri";
+      const expected = `proxy-media?url=${encodeURIComponent("https://mock.stream.origin.se/hls/vods/asset41/video-variants/variant_3.m3u8")}&delay=${encodeURIComponent(
+        "[{i:3,ms:200}]"
+      )}`;
+
+      // Assert
+      expect(actual).toEqual(expected);
+    });
+
+    it("should return correct format with value in all parameters, when source url is already an absolut url", () => {
+      // Arrange
+      const itemUri = "https://different.origin.se/hls/variant_3.m3u8";
+      const urlSearchParams = new URLSearchParams("url=https://mock.stream.origin.se/hls/master.m3u8&delay=[{i:3,ms:200}]");
+
+      // Act
+      const actual = proxyPathBuilder(itemUri, urlSearchParams, "proxy-media");
+      const expected = `proxy-media?url=${encodeURIComponent("https://different.origin.se/hls/variant_3.m3u8")}&delay=${encodeURIComponent("[{i:3,ms:200}]")}`;
+
+      // Assert
+      expect(actual).toEqual(expected);
+    });
+
+    it("should return correct format with value in all parameters, when source url string has '../'", () => {
+      // Arrange
+      const itemUri = "../../../pathB/path3/variant_3.m3u8";
+      const urlSearchParams = new URLSearchParams("url=https://mock.stream.origin.se/hls/pathA/path1/path2/master.m3u8&delay=[{i:3,ms:200}]");
+
+      // Act
+      const actual = proxyPathBuilder(itemUri, urlSearchParams, "proxy-media");
+      const expected = `proxy-media?url=${encodeURIComponent("https://mock.stream.origin.se/hls/pathB/path3/variant_3.m3u8")}&delay=${encodeURIComponent("[{i:3,ms:200}]")}`;
 
       // Assert
       expect(actual).toEqual(expected);
