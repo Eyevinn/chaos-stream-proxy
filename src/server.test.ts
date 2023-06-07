929eaa751a77df0243f585bfeef235c85736ceb4
VolcanoCookies <franek.kg@gmail.com>
2023-06-07 18:09:55 +0200

    feat: optionally require JWT token to access api, logs requests

--- src/server.test.ts
+++ src/server.test.ts
@@ -7,10 +7,17 @@ import {
 } from './segments/constants';
 
 describe('Chaos Stream Proxy server', () => {
+  const env = process.env;
   let app = null;
   beforeEach(() => {
+    jest.resetModules();
+    process.env = { ...env };
     app = fastify();
     registerRoutes(app);
+  });
+
+  afterEach(() => {
+    process.env = env;
   });
 
   it.each([HLS_PROXY_MASTER, HLS_PROXY_MEDIA, SEGMENTS_PROXY_SEGMENT])(
@@ -24,4 +31,25 @@ describe('Chaos Stream Proxy server', () => {
       );
     }
   );
+
+  it('requires token when running with env JWT_SECRET set', async () => {
+    process.env.JWT_SECRET = 'somesecret';
+    const appInternal = fastify();
+    registerRoutes(appInternal);
+    const invalidResponse = await appInternal.inject('/?token=invalid');
+    expect(invalidResponse.statusCode).toEqual(401);
+
+    const validResponse = await appInternal.inject(
+      '/?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb21wYW55IjoidGVzdGNvbXBhbnkiLCJlbWFpbCI6InRlc3RAZW1haWwuY29tIiwiaWF0IjoxNjg2MTUzMzU5fQ.wHnzxMdoPZlzdU0GDCzEwd5lnEmq-rX2Ew0yODxqlzg'
+    );
+    expect(validResponse.statusCode).toEqual(200);
+  });
+
+  it('ignores token when running without env JWT_SECRET set', async () => {
+    process.env.JWT_SECRET = undefined;
+    const appInternal = fastify();
+    registerRoutes(appInternal);
+    const response = await appInternal.inject('/?token=invalid');
+    expect(response.statusCode).toEqual(200);
+  });
 });
